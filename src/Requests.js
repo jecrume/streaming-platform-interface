dd6af20356847576e4bc95395d4767b0143e9a7d
jecrume <jecrume@gmail.com>
2021-03-05 20:24:55 -0500

    Banner complete with background image, buttons and truncated description

---
+++ src/Requests.js
@@ -0,0 +1,23 @@
+
+
+const API_KEY= process.env.REACT_APP_TMDB_API_KEY;
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movies/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchAdventureMovies:`/discover/movie?api_key=${API_KEY}&with_genres=12`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchSciFiMovies:`/discover/movie?api_key=${API_KEY}&with_genres=878`,
+    fetchFantasyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=14`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchMysteryMovies: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+    fetchCrimeMovies:`/discover/movie?api_key=${API_KEY}&with_genres=80`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchThrillerMovies: `/discover/movie?api_key=${API_KEY}&with_genres=53`,
+
+}
+
+export default requests;
