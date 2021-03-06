2cd374f3931930ba5c940931df651085bedfc66c
jecrume <jecrume@gmail.com>
2021-03-05 23:16:50 -0500

    Rows created but need to be styled in Rows.css

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./HomeScreen.css";
 import Nav from "./Nav";
 import Banner from "./Banner";
+import requests from "./Requests";
+import Row from "./Row"
 
 
 function HomeScreen() {
@@ -14,6 +16,22 @@ function HomeScreen() {
             <Banner />
 
             {/*Row */}
+
+            <Row title= "NETFLIX ORIGINALS" fetchUrl ={requests.fetchNetflixOriginals} isLargeRow />
+            <Row title= "Trending" fetchUrl ={requests.fetchTrending}/>
+            
+            <Row title= "Top Rated" fetchUrl ={requests.fetchTopRated}/>
+            <Row title= "Action" fetchUrl ={requests.fetchActionMovies}/>
+            <Row title= "Adventure" fetchUrl ={requests.fetchAdventureMovies}/>
+            <Row title= "Comedy" fetchUrl ={requests.fetchComedyMovies}/>
+            <Row title= "Sci-Fi" fetchUrl ={requests.fetchSciFiMovies}/>
+            <Row title= "Fantasy" fetchUrl ={requests.fetchFantasyMovies}/>
+            <Row title= "Horror" fetchUrl ={requests.fetchHorrorMovies}/>
+            <Row title= "Mystery" fetchUrl ={requests.fetchMysteryMovies}/>
+            <Row title= "Romance" fetchUrl ={requests.fetchRomanceMovies}/>
+            <Row title= "Thriller" fetchUrl ={requests.fetchThrillerMovies}/>
+            <Row title= "Crime" fetchUrl ={requests.fetchCrimeMovies}/>
+            <Row title= "Documentary" fetchUrl ={requests.fetchDocumentaries}/>
 
         
             
